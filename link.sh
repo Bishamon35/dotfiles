f5751820ff447cfb9a1238b8da82061a5e501dfd
Bishamon35 <gachatrash61@gmail.com>
2022-06-26 14:24:49 -0500

    Added mpv files

--- link.sh
+++ link.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ln -sbf ./.vimrc ~/.vimrc
-ln -sbf ./.zshrc ~/.zshrc
-ln -sbf ./.gitconfig ~/.gitconfig 
-ln -sbf ./lf ~/.config/lf
-ln -sbf ./mpd.conf ~/.config/mpd/
-ln -sbf ./ncmpcpp ~/.config/ncmpcpp
-ln -sbf ./init.vim ~/.config/nvim/   
-ln -sbf ./plug.vim ~/.local/share/nvim/site/autoload/plug.vim
-ln -sbf ./plug.vim ~/.vim/autoload/plug.vim
+ln -sbf ~/Git/dotfiles/lf ~/.config/lf
+ln -sbf ~/Git/dotfiles/mpv ~/.config/mpv
+ln -sbf ~/Git/dotfiles/ncmpcpp ~/.config/ncmpcpp
+ln -sbf ~/Git/dotfiles/.gitconfig ~/.gitconfig 
+ln -sbf ~/Git/dotfiles/.vimrc ~/.vimrc
+ln -sbf ~/Git/dotfiles/.zshrc ~/.zshrc
+ln -sbf ~/Git/dotfiles/init.vim ~/.config/nvim/   
+ln -sbf ~/Git/dotfiles/mpd.conf ~/.config/mpd/
+ln -sbf ~/Git/dotfiles/plug.vim ~/.local/share/nvim/site/autoload/plug.vim
+ln -sbf ~/Git/dotfiles/plug.vim ~/.vim/autoload/plug.vim
 
